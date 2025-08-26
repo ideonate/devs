0c74daa6b2ebe9d06957059510007c599b2f5ac3
Dan Lester <dan@ideonate.com>
2025-08-26 09:51:41 +0100

    remove firewall

--- packages/common/devs_common/templates/scripts/post-create-wrapper.sh
+++ packages/common/devs_common/templates/scripts/post-create-wrapper.sh
@@ -33,7 +33,7 @@ run_step() {
 
 # Run each step with error handling
 run_step "setup-devs-env.sh" "/usr/local/bin/setup-devs-env.sh"
-run_step "init-firewall.sh" "sudo /usr/local/bin/init-firewall.sh"
+#run_step "init-firewall.sh" "sudo /usr/local/bin/init-firewall.sh"
 run_step "setup-workspace.sh" "/usr/local/bin/setup-workspace.sh"
 run_step "start-services.sh" "sudo /usr/local/bin/start-services.sh"
 
