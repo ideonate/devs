8e3a90ad88f22158cce3ae52b8e0ea0f653ae29f
Dan Lester <dan@ideonate.com>
2025-09-15 13:06:32 +0100

    latest devcontainer template

--- packages/common/devs_common/templates/scripts/post-create-wrapper.sh
+++ packages/common/devs_common/templates/scripts/post-create-wrapper.sh
@@ -32,6 +32,7 @@ run_step() {
 }
 
 # Run each step with error handling
+run_step "setup-vscode-cache" "sudo /usr/local/bin/setup-vscode-cache.sh"
 run_step "setup-devs-env.sh" "/usr/local/bin/setup-devs-env.sh"
 #run_step "init-firewall.sh" "sudo /usr/local/bin/init-firewall.sh"
 run_step "setup-workspace.sh" "/usr/local/bin/setup-workspace.sh"
